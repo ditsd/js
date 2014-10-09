41c9b6a57fb699a26e2667920d1b688a52c9fe01
ditsd <df@gmail.com>
2014-10-09 14:25:14 -0400

    6

--- src/object.js
+++ src/object.js
@@ -4,7 +4,7 @@ NS.myFunction = {
     //empty stuff array, filled during initialization
     stuff: [],
     init: function init() {
-        this.stuff.push('Testin5');
+        this.stuff.push('Testin6');
     },
     reset: function reset() {
         this.stuff = [];
