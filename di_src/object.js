e0c41647ea0ddab8ef1c76cf6abcbe431557dbca
ditsd <df@gmail.com>
2014-12-03 11:02:40 -0500

    01

--- di_src/object.js
+++ di_src/object.js
@@ -1,17 +1,3 @@
-if (typeof NS == 'undefined') { NS = {}; }
- 
-NS.myFunction = {
-    //empty stuff array, filled during initialization
-    stuff: [],
-    init: function init() {
-        this.stuff.push('Testing.');
-    },
-    
-    reset: function reset() {
-        this.stuff = [];
-    },
-    //will add new functionality here later
-};
-
-
-NS.myFunction.init();
+function helloWorld() {
+    return "Hello world!";
+}
